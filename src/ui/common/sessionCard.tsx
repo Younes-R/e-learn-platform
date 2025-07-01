7f95ef7147bff014d29f757b3be6d9aa68f2eecf
younes-R <y_ounis@estin.dz>
2025-07-01 23:23:01 +0100

    created SessionCard

---
+++ src/ui/common/sessionCard.tsx
@@ -0,0 +1,26 @@
+import styles from "./sessionCard.module.css";
+import Link from "next/link";
+
+export default function SessionCard() {
+  return (
+    <li className={styles["card"]}>
+      <h4 className={styles["card__header"]}>Language Theory</h4>
+      <ul className={styles["card__time-info-list"]}>
+        <li>Sun 04, Feb 2024</li>
+        <li>10:00 - 11:00</li>
+        <li>
+          <span>Online</span>
+        </li>
+      </ul>
+      <ul className={styles["card__info-list"]}>
+        <li>By Dr. Zedek</li>
+        <li>
+          <span>200DA</span>
+        </li>
+      </ul>
+      <button>
+        <Link href={"/sessions"}>View Session</Link>
+      </button>
+    </li>
+  );
+}
