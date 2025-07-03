05d31995c52da26c6171c2642f93cbb668344def
younes-R <y_ounis@estin.dz>
2025-07-03 14:29:59 +0100

    added admin courses page

---
+++ src/app/admin/courses/page.tsx
@@ -0,0 +1,54 @@
+import styles from "./page.module.css";
+import Courses from "@/ui/student/Courses/courses";
+
+export default function Page() {
+  return (
+    <main className={styles.main}>
+      <h2>Courses</h2>
+      <Courses
+        coursesDataSegments={[
+          {
+            cid: "1232lknasd",
+            title: "Course Title",
+            description: "This course is for Electronics 02.",
+            enrolledStudentsNumber: 34,
+            price: 2000,
+            module: "Electronics",
+            level: "1CS",
+            documents: [
+              { title: "ererewrwer", uri: "fsdfdfdf" },
+              { title: "ererewrwer", uri: "fsdfdfdf" },
+              { title: "ererewrwer", uri: "fsdfdfdf" },
+            ],
+          },
+          {
+            cid: "234kjnffdsf",
+            title: "Electronics 2",
+            description:
+              "This Course is for Electronics 2. It is about transistors and their types, behaviors and evolutions through the industry.",
+            enrolledStudentsNumber: 12,
+            price: 2500,
+            module: "Electronics 02",
+            level: "2CP",
+            documents: [
+              { title: "Transistors.pdf", uri: "asdsdasdsdasd" },
+              { title: "Diodes.pdf", uri: "asdasdadsds" },
+            ],
+          },
+          {
+            cid: "546809402woend",
+            title: "Course Title",
+            description: "This course is for Electronics 02.",
+            enrolledStudentsNumber: 14,
+          },
+          {
+            cid: "978mvsdvfv",
+            title: "Course Title",
+            description: "This course is for Electronics 02.",
+            enrolledStudentsNumber: 24,
+          },
+        ]}
+      />
+    </main>
+  );
+}
