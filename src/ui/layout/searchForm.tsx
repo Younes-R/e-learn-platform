fa930b517a48609556e98387dac6f4db17cfdb41
younes-R <y_ounis@estin.dz>
2025-07-08 16:28:22 +0100

    improved SearchFrom: conditional rendering, flex wrap on and using select input

--- src/ui/layout/searchForm.tsx
+++ src/ui/layout/searchForm.tsx
@@ -7,6 +7,10 @@ import FilterIcon from "../icons/filterIcon";
 export default function SearchForm() {
   const [isToggled, setIsToggled] = useState<boolean>(false);
 
+  const [teacherToggle, setTeacherToggle] = useState<boolean>(false);
+  const [courseToggle, setCourseToggle] = useState<boolean>(false);
+  const [sessionToggle, setSessionToggle] = useState<boolean>(false);
+
   return (
     <form
       className={styles["search-form"]}
@@ -33,45 +37,65 @@ export default function SearchForm() {
           <div>
             <label className={styles["search-form__extended-part__search-label"]}>
               Search:
-              <label
-                className={styles["search-form__extended-part__principal-label__teacher-label"]}
-                htmlFor="teacher-option"
-              >
-                Teacher
-                <input
-                  type="checkbox"
-                  name="resourceOptions"
-                  value="teacher"
-                  id="teacher-option"
-                  onChange={() => console.log("teacher-option toggled!")}
-                />
-              </label>
-              <label
-                className={styles["search-form__extended-part__principal-label__course-label"]}
-                htmlFor="course-option"
-              >
-                Course
-                <input
-                  type="checkbox"
-                  name="resourceOptions"
-                  value="course"
-                  id="course-option"
-                  onChange={() => console.log("course-option toggled!")}
-                />
-              </label>
-              <label
-                className={styles["search-form__extended-part__principal-label__session-label"]}
-                htmlFor="session-option"
-              >
-                Session
-                <input
-                  type="checkbox"
-                  name="resourceOptions"
-                  value="session"
-                  id="session-option"
-                  onChange={() => console.log("session-option toggled!")}
-                />
-              </label>
+              <p>
+                <label
+                  className={`${styles["search-form__extended-part__principal-label__teacher-label"]} ${
+                    teacherToggle ? styles["search-form__extended-part__principal-label__label--toggled"] : ""
+                  } `}
+                  htmlFor="teacher-option"
+                >
+                  Teacher
+                  <input
+                    type="checkbox"
+                    name="resourceOptions"
+                    value="teacher"
+                    id="teacher-option"
+                    onChange={() => {
+                      console.log("teacher-option toggled!");
+                      setTeacherToggle(!teacherToggle);
+                    }}
+                    checked={teacherToggle}
+                  />
+                </label>
+                <label
+                  className={`${styles["search-form__extended-part__principal-label__course-label"]} ${
+                    courseToggle ? styles["search-form__extended-part__principal-label__label--toggled"] : ""
+                  }`}
+                  htmlFor="course-option"
+                >
+                  Course
+                  <input
+                    type="checkbox"
+                    name="resourceOptions"
+                    value="course"
+                    id="course-option"
+                    onChange={() => {
+                      console.log("course-option toggled!");
+                      setCourseToggle(!courseToggle);
+                    }}
+                    checked={courseToggle}
+                  />
+                </label>
+                <label
+                  className={`${styles["search-form__extended-part__principal-label__session-label"]} ${
+                    sessionToggle ? styles["search-form__extended-part__principal-label__label--toggled"] : ""
+                  }`}
+                  htmlFor="session-option"
+                >
+                  Session
+                  <input
+                    type="checkbox"
+                    name="resourceOptions"
+                    value="session"
+                    id="session-option"
+                    onChange={() => {
+                      console.log("session-option toggled!");
+                      setSessionToggle(!sessionToggle);
+                    }}
+                    checked={sessionToggle}
+                  />
+                </label>
+              </p>
             </label>
             <label
               className={styles["search-form__extended-part__principal-label"]}
@@ -85,53 +109,66 @@ export default function SearchForm() {
               />
             </label>
           </div>
-          <div>
-            <label
-              className={styles["search-form__extended-part__principal-label"]}
-              htmlFor="level"
-            >
-              Level:
-              <input
-                type="text"
-                name="level"
-                id="level"
-              />
-            </label>
-            <label
-              className={styles["search-form__extended-part__principal-label"]}
-              htmlFor="price"
-            >
-              Price:
-              <input
-                type="number"
-                name="price"
-                id="price"
-              />
-            </label>
-          </div>
-          <div>
-            <label
-              className={styles["search-form__extended-part__principal-label"]}
-              htmlFor="type"
-            >
-              Type:
-              <input
-                type="text"
-                name="type"
-                id="type"
-              />
-            </label>
-            <label
-              className={styles["search-form__extended-part__principal-label"]}
-              htmlFor="location"
-            >
-              Location:
-              <input
-                type="text"
-                name="location"
-                id="location"
-              />
-            </label>
+          {courseToggle || sessionToggle ? (
+            <>
+              <div>
+                <label
+                  className={styles["search-form__extended-part__principal-label"]}
+                  htmlFor="level"
+                >
+                  Level:
+                  <input
+                    type="text"
+                    name="level"
+                    id="level"
+                  />
+                </label>
+                <label
+                  className={styles["search-form__extended-part__principal-label"]}
+                  htmlFor="price"
+                >
+                  Price:
+                  <input
+                    type="number"
+                    name="price"
+                    id="price"
+                  />
+                </label>
+              </div>
+            </>
+          ) : null}
+          {sessionToggle ? (
+            <>
+              <div>
+                <label
+                  className={styles["search-form__extended-part__principal-label"]}
+                  htmlFor="type"
+                >
+                  Type:
+                  <select
+                    name="type"
+                    id="type"
+                  >
+                    <option value="online">Online</option>
+                    <option value="offline">Offline</option>
+                  </select>
+                </label>
+                <label
+                  className={styles["search-form__extended-part__principal-label"]}
+                  htmlFor="location"
+                >
+                  Location:
+                  <input
+                    type="text"
+                    name="location"
+                    id="location"
+                  />
+                </label>
+              </div>{" "}
+            </>
+          ) : null}
+          <div className={styles["search-form__extended-part__main-action__div"]}>
+            <button className={styles["search-form__extended-part__main-action__div__button"]}>Apply</button>
           </div>
         </div>
       ) : null}
