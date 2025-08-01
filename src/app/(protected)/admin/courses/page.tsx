0af9efa3458df66050985f66140b8d86d7018f2d
younes-R <y_ounis@estin.dz>
2025-08-01 17:47:25 +0100

    updated Courses, fixed a bug in EditPanel, protected and linked admin courses page to the DAL

--- src/app/(protected)/admin/courses/page.tsx
+++ src/app/(protected)/admin/courses/page.tsx
@@ -1,42 +1,17 @@
 import styles from "./page.module.css";
 import Courses from "@/ui/teacher/Courses/courses";
+import { verifyRefreshToken, verifyRoles } from "@/lib/utils";
+import { getCourses } from "@/database/dal/admin";
 
-export default function Page() {
+export default async function Page() {
+  await verifyRefreshToken();
+  await verifyRoles(["admin"]);
+  const courses = await getCourses();
+
   return (
     <main className={styles.main}>
       <h2>Courses</h2>
-      <Courses
-        coursesDataSegments={[
-          {
-            cid: "1232lknasd",
-            title: "Course Title",
-            description: "This course is for Electronics 02.",
-            enrolledStudentsNumber: 34,
-            price: 2000,
-            module: "Electronics",
-            level: "1CS",
-            documents: [
-              { title: "ererewrwer", uri: "fsdfdfdf" },
-              { title: "ererewrwer", uri: "fsdfdfdf" },
-              { title: "ererewrwer", uri: "fsdfdfdf" },
-            ],
-          },
-          {
-            cid: "234kjnffdsf",
-            title: "Electronics 2",
-            description:
-              "This Course is for Electronics 2. It is about transistors and their types, behaviors and evolutions through the industry.",
-            enrolledStudentsNumber: 12,
-            price: 2500,
-            module: "Electronics 02",
-            level: "2CP",
-            documents: [
-              { title: "Transistors.pdf", uri: "asdsdasdsdasd" },
-              { title: "Diodes.pdf", uri: "asdasdadsds" },
-            ],
-          },
-        ]}
-      />
+      <Courses coursesDataSegments={courses} />
     </main>
   );
 }
--- src/database/dal/admin.ts
+++ src/database/dal/admin.ts
@@ -2,6 +2,55 @@ import { neon } from "@neondatabase/serverless";
 
 const sql = neon(process.env.DATABASE_URL!);
 
+export async function getCourses() {
+  try {
+    const coursesDataSegments =
+      (await sql`SELECT cid, title, description, price, module, c_year AS level FROM courses`) as Array<{
+        cid: string;
+        title: string;
+        description: string;
+        price: number;
+        module: string;
+        level: string;
+      }>;
+    if (!(coursesDataSegments && coursesDataSegments.length > 0)) {
+      return null;
+    }
+    const documentsData = (await sql`SELECT doc_title AS title, file_id AS uri, cid FROM documents`) as Array<{
+      title: string;
+      uri: string;
+      cid: string;
+    }>;
+    if (!(documentsData && documentsData.length > 0)) {
+      console.warn("All courses have no documents!");
+    }
+    const enrolledStudentsCountArray =
+      (await sql`SELECT courses.cid, COUNT(payments.id) AS "studentsCount" FROM courses LEFT JOIN payments ON courses.cid = payments.cid GROUP BY courses.cid`) as Array<{
+        cid: string;
+        studentsCount: string;
+      }>;
+
+    const coursesData = coursesDataSegments.map((course) => {
+      const documents = documentsData.filter((document) => document.cid === course.cid);
+      const enrolledStudentsNumber = enrolledStudentsCountArray.find((record) => record.cid === course.cid);
+      return {
+        ...course,
+        documents: documents,
+        enrolledStudentsNumber: Number(enrolledStudentsNumber?.studentsCount),
+      };
+    });
+
+    return coursesData;
+  } catch (err: any) {
+    console.error(`[Database error]: 
+      msg: ${err.message}
+      routine: ${err.routine}
+      hint: ${err.hint}
+    `);
+    throw new Error("[getCourses]: Failed to get courses.", { cause: err });
+  }
+}
+
 export async function getUsers(usersType: "student" | "teacher" | "moderator") {
   try {
     if (!["student", "teacher", "moderator"].includes(usersType))
